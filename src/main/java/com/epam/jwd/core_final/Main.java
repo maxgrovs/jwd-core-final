9bf1724ffa814d60901cdb52c45c21b5a7fd6064
maxgrovs <maxgrovs@gmail.com>
2020-12-31 00:36:59 +0300

    test

--- src/main/java/com/epam/jwd/core_final/Main.java
+++ src/main/java/com/epam/jwd/core_final/Main.java
@@ -5,7 +5,7 @@ import com.epam.jwd.core_final.context.Application;
 public class Main {
 
     public static void main(String[] args) {
-        Application.start();
-        System.out.println();
+        //Application.start();
+
     }
 }
---
+++ src/test/java/com/epam/jwd/core_final/MainTest.java
@@ -0,0 +1,15 @@
+package com.epam.jwd.core_final;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+
+        System.out.println("djdjdj");
+
+    }
+}
